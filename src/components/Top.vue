2bf1bf3e591e8e6115fe02d13f6908d612d13f5b
soul <celestiallie@mgail.com>
2018-10-24 12:05:45 +0800

    take away nav tag

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,22 +1,20 @@
 <template>
   <div class="top">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mx-auto">
-          <li class="nav-item" @click="active('overview')">
-            <router-link to="/" 
-            :class="{ active: isActive('overview') }"
-            >總覽 </router-link>
-          </li> 
-          <li class="nav-item" @click="active('insert')">
-            <router-link to="/new" 
-            :class="{ active: isActive('insert') }"
-            >新增 </router-link>
-          </li>
-          new
-        </ul>
-      </div>
-    </nav>
+    <div>
+      <ul>
+        <li @click="active('overview')">
+          <router-link to="/" 
+          :class="{ active: isActive('overview') }"
+          >總覽 </router-link>
+        </li> 
+        <li @click="active('insert')">
+          <router-link to="/new" 
+          :class="{ active: isActive('insert') }"
+          >新增 </router-link>
+        </li>
+        new
+      </ul>
+    </div>
   </div>
 </template>
 
